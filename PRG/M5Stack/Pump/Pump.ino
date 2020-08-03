d37fe96c07023f38ea1ad7abbb1d165516049710
extsui <en422641@s.okayama-u.ac.jp>
2020-08-03 23:44:26 +0900

    (Pump) 簡易コマンド解析を作成 #16

--- PRG/M5Stack/Pump/Pump.ino
+++ PRG/M5Stack/Pump/Pump.ino
@@ -24,20 +24,24 @@ void setup()
 
 constexpr int ReceiveBufferSize = 256;
 
-static byte g_receiveBuffer[ReceiveBufferSize];
+static uint8_t g_receiveBuffer[ReceiveBufferSize];
 static int g_receiveBufferIndex = 0;
 
 void loop()
 {
   if (Serial.available()) {
-    byte data = Serial.read();
+    uint8_t data = Serial.read();
 		g_receiveBuffer[g_receiveBufferIndex] = data;
 		g_receiveBufferIndex++;
 
 		if (data == '\n') {
 			// コマンド確定
 			g_receiveBuffer[g_receiveBufferIndex - 1] = '\0';
-		  ExecuteCommand(g_receiveBuffer);
+		  int result = ExecuteCommand(g_receiveBuffer);
+      if (result == -1) {
+        Serial.printf("Error! [%s]\n", g_receiveBuffer);
+      }
+
       memset(g_receiveBuffer, 0, sizeof(g_receiveBuffer));
 		  g_receiveBufferIndex = 0;
 		}
@@ -47,9 +51,110 @@ void loop()
   // ...
 }
 
-void ExecuteCommand(const byte *command)
+// 10 進数にした時の文字列長
+int GetDigitLength(uint8_t value)
+{
+  if (value < 10) {
+    return 1;
+  } else if (value < 100) {
+    return 2;
+  } else {
+    return 3;
+  }
+}
+
+int ExecuteCommand(const uint8_t *command)
 {
   // TODO: コマンド解析
+  // テストサンプルコマンド
+  // - "0 pattern 2 11 100 1" --> [ 0x00, 0x10, 0x02, 0x0b, 0x64, 0x01 ]
+  // - "4 bright 3 8"         --> [ 0x04, 0x20, 0x03, 0x08 ]
+  // - "7 bright 15"          --> [ 0x07, 0x21, 0x0F ] ★場合分けが面倒なので後回し★
 
-  Serial.printf("[%s]\n", command);
+  // 共通
+  uint8_t addr = 0;
+  uint8_t commandId = 0;
+  uint8_t brickId = 0;
+
+  // pattern
+  uint8_t patternId = 0;
+  uint8_t timing = 0;
+  uint8_t isRepeat = 0;
+
+  // brick
+  uint8_t brightness = 0;
+
+  // ツールからの受信を想定しているので過度のエラーチェックは不要
+  char *str = (char*)command;
+  int pos = 0;
+  addr = (str[pos] - '0');
+  pos += 1 + 1;
+
+  if (strncmp(&str[pos], "pattern", 7) == 0) {
+    commandId = 0x10;
+    pos += 7 + 1;
+
+    brickId = str[pos] - '0';
+    pos += 1 + 1;
+
+    patternId = atoi(&str[pos]);
+    pos += GetDigitLength(patternId) + 1;
+
+    timing = atoi(&str[pos]);
+    pos += GetDigitLength(timing) + 1;
+
+    isRepeat = str[pos] - '0';
+    pos += 1;
+
+    Serial.printf("addr      = 0x%02x\n", addr);
+    Serial.printf("commandId = 0x%02x\n", commandId);
+    Serial.printf("brickId   = 0x%02x\n", brickId);
+    Serial.printf("patternId = 0x%02x\n", patternId);
+    Serial.printf("timing    = 0x%02x\n", timing);
+    Serial.printf("isRepeat  = 0x%02x\n", isRepeat);
+
+    if (str[pos] != '\0') {
+      return -1;
+    }
+
+  } else if (strncmp(&str[pos], "bright", 6) == 0) {
+    commandId = 0x20;
+    pos += 6 + 1;
+
+    brickId = str[pos] - '0';
+    pos += 1 + 1;
+
+    brightness = atoi(&str[pos]);
+    pos += GetDigitLength(brightness);
+
+    Serial.printf("addr       = 0x%02x\n", addr);
+    Serial.printf("commandId  = 0x%02x\n", commandId);
+    Serial.printf("brickId    = 0x%02x\n", brickId);
+    Serial.printf("brightness = 0x%02x\n", brightness);
+
+    if (str[pos] != '\0') {
+      return -1;
+    }
+
+  } else {
+    return -1;
+  }
+
+  // 解析成功
+  switch (commandId) {
+    case 0x10:
+      Serial.printf("[ 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x ]\n",
+        addr, commandId, brickId, patternId, timing, isRepeat);
+      break;
+    case 0x20:
+      Serial.printf("[ 0x%02x 0x%02x 0x%02x 0x%02x ]\n",
+        addr, commandId, brickId, brightness);
+      break;
+    default:
+      return -1;
+  }
+
+  Serial.printf("[%s]\n", str);
+
+  return 0;
 }
