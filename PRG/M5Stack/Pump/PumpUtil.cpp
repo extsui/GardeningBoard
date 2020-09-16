05c1e624607275e82ca020a0d17cfb9d47fb563f
extsui <en422641@s.okayama-u.ac.jp>
2020-09-17 00:35:36 +0900

    (Pump) 生データ送信コマンドの実装とテスト追加 #16

--- PRG/M5Stack/Pump/PumpUtil.cpp
+++ PRG/M5Stack/Pump/PumpUtil.cpp
@@ -1,6 +1,71 @@
 #include "PumpUtil.hpp"
+#include <cstdint>
+#include <cassert>
+#include <cstddef>
+#include <string.h>
 
-int PumpUtil::add(int a, int b)
+/**
+ * 16 進表記の 2 文字を 1 バイトに変換する。
+ * 成功した場合は outValue に変換後の値が代入される。
+ * @return 0: 成功 / -1: 失敗
+ */
+int PumpUtil::ToHex(uint8_t *outValue, char hi, char lo)
 {
-    return a + b;
+    uint8_t h, l;
+
+    assert(outValue != NULL);
+    
+    if (('a' <= hi) && (hi <= 'f')) {
+        h = hi - 'a' + 10;
+    } else if (('A' <= hi) && (hi <= 'F')) {
+        h = hi - 'A' + 10;
+    } else if (('0' <= hi) && (hi <= '9')) {
+        h = hi - '0';
+    } else {
+        return -1;
+    }
+    
+    if (('a' <= lo) && (lo <= 'f')) {
+        l = lo - 'a' + 10;
+    } else if (('A' <= lo) && (lo <= 'F')) {
+        l = lo - 'A' + 10;
+    } else if (('0' <= lo) && (lo <= '9')) {
+        l = lo - '0';
+    } else {
+        return -1;
+    }
+    
+    *outValue = (h << 4) | (l & 0x0F);
+    return 0;
+}
+
+/**
+ * 生データ送信コマンドを解析する。
+ * @return 成功: 解釈できたバイト数 / 失敗: -1
+ */
+int PumpUtil::ParseRawSendCommand(const char *command, uint8_t *array, int arrayLength)
+{
+    int result = 0;
+    const char *prefix = "@send ";
+    const int prefixLength = strlen(prefix);
+
+    assert(command != NULL);
+    assert(array != NULL);
+    if (strncmp(&command[0], prefix, prefixLength) != 0) {
+        return -1;
+    }
+
+    // arrayLength を越えるか command が最後まで到達したら終了
+    int readPos = prefixLength;
+    int writePos = 0;
+    while ((writePos < arrayLength) && (command[readPos] != '\0')) {
+        result = ToHex(&array[writePos], command[readPos], command[readPos + 1]);
+        if (result == -1) {
+            return -1;
+        }
+        readPos += 2;
+        writePos++;
+    }
+
+    return writePos;
 }
--- PRG/M5Stack/Pump/PumpUtil.hpp
+++ PRG/M5Stack/Pump/PumpUtil.hpp
@@ -1,10 +1,13 @@
 #ifndef PUMP_UTIL_HPP
 #define PUMP_UTIL_HPP
 
+#include <stdint.h>
+
 class PumpUtil
 {
 public:
-    static int add(int a, int b);
+    static int ToHex(uint8_t *outValue, char hi, char lo);
+    static int ParseRawSendCommand(const char *command, uint8_t *array, int arrayLength);
 };
 
 #endif /* PUMP_UTIL_HPP */
