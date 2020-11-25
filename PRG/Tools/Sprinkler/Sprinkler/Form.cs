2daba2c751099432c6eb8d2bfc27525d6e869053
extsui <en422641@s.okayama-u.ac.jp>
2020-11-26 00:43:25 +0900

    (Sprinkler) ベース作成 #34

---
+++ PRG/Tools/Sprinkler/Sprinkler/Form.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Sprinkler
+{
+    public partial class Sprinkler : Form
+    {
+        public Sprinkler()
+        {
+            InitializeComponent();
+
+            var ports = SerialPort.GetPortNames();
+        }
+    }
+}
