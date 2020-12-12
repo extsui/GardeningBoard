c75bb26fc8591a2009e998a548a182dc15cb65aa
extsui <en422641@s.okayama-u.ac.jp>
2020-12-12 16:10:44 +0900

    (Sprinkler) 庭コマンドのスクリプト部分を分離 #36

--- PRG/Tools/Sprinkler/Sprinkler/Form.cs
+++ PRG/Tools/Sprinkler/Sprinkler/Form.cs
@@ -3,9 +3,6 @@ using System.Diagnostics;
 using System.IO.Ports;
 using System.Windows.Forms;
 using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Linq;
 
 namespace Sprinkler
 {
@@ -23,8 +20,8 @@ namespace Sprinkler
             m_serialSendCount++;
         }
 
-        private Garden m_garden;
         private SerialPort m_serialPort;
+        private GardenScripter m_scripter;
 
         /// <summary>
         /// 通信開始時の時刻
@@ -34,7 +31,7 @@ namespace Sprinkler
         public Sprinkler()
         {
             InitializeComponent();
-            m_garden = new Garden();
+            m_scripter = new GardenScripter(SerialSendAsync);
         }
 
         private void Sprinkler_Load(object sender, EventArgs e)
@@ -67,7 +64,7 @@ namespace Sprinkler
 
         private void buttonTest_Click(object sender, EventArgs e)
         {
-            ExecuteTest();
+            m_scripter.ExecuteTest();
         }
 
         private void textBoxKeyInput_KeyDown(object sender, KeyEventArgs e)
@@ -78,11 +75,11 @@ namespace Sprinkler
                 //  q w e r t y u i o p @ [
                 //   a s d f g h j k l ; : ]
                 //    z x c v b n m , . / \
-                { Keys.A, SampleSequence   },
-                { Keys.S, SampleBrightness },
-                { Keys.D, SampleStress     },
-                { Keys.F, SampleCircle     },
-                { Keys.G, InternalMultiThreadTest },
+                { Keys.A, m_scripter.SampleSequence   },
+                { Keys.S, m_scripter.SampleBrightness },
+                { Keys.D, m_scripter.SampleStress     },
+                { Keys.F, m_scripter.SampleCircle     },
+                { Keys.G, m_scripter.InternalMultiThreadTest },
             };
 
             if (table.ContainsKey(e.KeyCode))
@@ -128,257 +125,6 @@ namespace Sprinkler
                     UpdateStatusBar();
                 }
             }
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////
-        //  コマンド部品
-        ////////////////////////////////////////////////////////////////////////////////
-
-        // TODO: 輝度指定もあり?
-        // TODO: PositionList の Not が欲しくなる
-        // TODO: Formation 毎にコマンド群 (クラス?) を定義する必要がありそう? (今回であれば Hexagon 用)
-        // TODO: シーケンスなるものは非同期版が欲しい
-
-        private void ExecuteCommand(List<string> commands)
-        {
-            Trace.Assert(commands != null);
-            string line = String.Join("", commands);
-            SerialSendAsync(line);
-        }
-
-        /// <summary>
-        /// 指定あれたアクションを非同期実行するラッパー
-        /// 対象のアクションは async 指定が不要
-        /// </summary>
-        private async void RunSequenceAsync(Action action)
-        {
-            await Task.Run(action);
-        }
-
-        private void CommandRegister()
-        {
-            ExecuteCommand(m_garden.MakeRegisterCommand());
-        }
-
-        private void CommandTurnOffAll()
-        {
-            ExecuteCommand(m_garden.MakePatternCommand(Position.Hexagon.All, OperationTarget.Both, BrickCommandArgs.PatternTurnOff));
-        }
-
-        private void CommandTurnOnAll()
-        {
-            ExecuteCommand(m_garden.MakePatternCommand(Position.Hexagon.All, OperationTarget.Both, BrickCommandArgs.PatternTurnOn));
-        }
-
-        private void CommandTurnOn(uint position, OperationTarget target)
-        {
-            ExecuteCommand(m_garden.MakePatternCommand(position, target, BrickCommandArgs.PatternTurnOn));
-        }
-
-        private void CommandTurnOff(uint position, OperationTarget target)
-        {
-            ExecuteCommand(m_garden.MakePatternCommand(position, target, BrickCommandArgs.PatternTurnOff));
-        }
-
-        private void SequentialCommandOneShot(uint position, OperationTarget target, int onTimeMsec)
-        {
-            CommandTurnOn(position, target);
-            Thread.Sleep(onTimeMsec);
-            CommandTurnOff(position, target);
-        }
-
-        private void SequentialCommandTurnOnInHexagonForm()
-        {
-            foreach (var position in Position.Hexagon.All)
-            {
-                ExecuteCommand(m_garden.MakePatternCommand(position, OperationTarget.Both, BrickCommandArgs.PatternTurnOn));
-                Thread.Sleep(200);
-            }
-        }
-
-        private void SequentialCommandTurnOffInHexagonForm()
-        {
-            foreach (var position in Position.Hexagon.All)
-            {
-                ExecuteCommand(m_garden.MakePatternCommand(position, OperationTarget.Both, BrickCommandArgs.PatternTurnOff));
-                Thread.Sleep(200);
-            }
-        }
-
-        private void CommandTurnOnInTriangleForm()
-        {
-            var TrianglePosition = new List<uint> { Position.Hexagon.Up, Position.Hexagon.RightDown, Position.Hexagon.LeftDown };
-            ExecuteCommand(m_garden.MakePatternCommand(TrianglePosition, OperationTarget.TileOnly, BrickCommandArgs.PatternTurnOn));
-        }
-
-        private void CommandTurnOnInReverseTriangleForm()
-        {
-            var ReverseTrianglePosition = new List<uint> { Position.Hexagon.Down, Position.Hexagon.LeftUp, Position.Hexagon.RightUp };
-            ExecuteCommand(m_garden.MakePatternCommand(ReverseTrianglePosition, OperationTarget.TileOnly, BrickCommandArgs.PatternTurnOn));
-        }
-
-        private void SequentialCommandTurnOnSmoothly(uint position, OperationTarget target, int delayMsec)
-        {
-            CommandTurnOn(position, target);
-
-            ExecuteCommand(m_garden.MakeBrightnessCommand(position, target, new BrickCommandArgs.Brightness(1)));
-            Thread.Sleep(delayMsec);
-            ExecuteCommand(m_garden.MakeBrightnessCommand(position, target, new BrickCommandArgs.Brightness(2)));
-            Thread.Sleep(delayMsec);
-            ExecuteCommand(m_garden.MakeBrightnessCommand(position, target, new BrickCommandArgs.Brightness(3)));
-            Thread.Sleep(delayMsec);
-        }
-
-        private void SequentialCommandTurnOffSmoothly(uint position, OperationTarget target, int delayMsec)
-        {
-            ExecuteCommand(m_garden.MakeBrightnessCommand(position, target, new BrickCommandArgs.Brightness(3)));
-            Thread.Sleep(delayMsec);
-            ExecuteCommand(m_garden.MakeBrightnessCommand(position, target, new BrickCommandArgs.Brightness(2)));
-            Thread.Sleep(delayMsec);
-            ExecuteCommand(m_garden.MakeBrightnessCommand(position, target, new BrickCommandArgs.Brightness(1)));
-            Thread.Sleep(delayMsec);
-
-            CommandTurnOff(position, target);
-        }
-
-        private void SequentialCommandOneShotSmoothly(uint position, OperationTarget target, int delayMsec)
-        {
-            SequentialCommandOneShotSmoothly(new List<uint> { position }, target, delayMsec);
-        }
-
-        private void SequentialCommandOneShotSmoothly(List<uint> positions, OperationTarget target, int delayMsec)
-        {
-            ExecuteCommand(m_garden.MakePatternCommand(positions, target, BrickCommandArgs.PatternTurnOn));
-
-            ExecuteCommand(m_garden.MakeBrightnessCommand(positions, target, new BrickCommandArgs.Brightness(1)));
-            Thread.Sleep(delayMsec);
-            ExecuteCommand(m_garden.MakeBrightnessCommand(positions, target, new BrickCommandArgs.Brightness(2)));
-            Thread.Sleep(delayMsec);
-            ExecuteCommand(m_garden.MakeBrightnessCommand(positions, target, new BrickCommandArgs.Brightness(3)));
-            Thread.Sleep(delayMsec);
-            ExecuteCommand(m_garden.MakeBrightnessCommand(positions, target, new BrickCommandArgs.Brightness(2)));
-            Thread.Sleep(delayMsec);
-            ExecuteCommand(m_garden.MakeBrightnessCommand(positions, target, new BrickCommandArgs.Brightness(1)));
-            Thread.Sleep(delayMsec);
-
-            ExecuteCommand(m_garden.MakePatternCommand(positions, target, BrickCommandArgs.PatternTurnOff));
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////
-        //  統合コマンド
-        ////////////////////////////////////////////////////////////////////////////////
-
-        private async void ExecuteTest()
-        {
-            // TODO: コマンド間の待ち時間を可変にしたい
-            // TODO: パターンの指定なども色々外注入にしたい
-            // TODO: 複数の Position を同時に制御したい (これは Garden 側で要対応案件)
-            await Task.Run(() =>
-            {
-                CommandRegister();
-                CommandTurnOffAll();
-                CommandTurnOnAll();
-                Thread.Sleep(1000);
-                CommandTurnOffAll();
-                Thread.Sleep(1000);
-                SequentialCommandTurnOnInHexagonForm();
-                Thread.Sleep(1000);
-                SequentialCommandTurnOffInHexagonForm();
-            });
-        }
-
-        private async void SampleSequence()
-        {
-            await Task.Run(() =>
-            {
-                CommandRegister();
-                for (int i = 0; i < 10; i++)
-                {
-                    CommandTurnOnInTriangleForm();
-                    Thread.Sleep(500);
-                    CommandTurnOffAll();
-                    CommandTurnOnInReverseTriangleForm();
-                    Thread.Sleep(500);
-                    CommandTurnOffAll();
-                }
-            });
-        }
-
-        private async void SampleBrightness()
-        {
-            await Task.Run(() =>
-            {
-                CommandRegister();
-                foreach (var position in Position.Hexagon.All)
-                {
-                    SequentialCommandOneShotSmoothly(position, OperationTarget.TileOnly, 50);
-                    Thread.Sleep(50);
-                    SequentialCommandOneShotSmoothly(position, OperationTarget.InsertedOnly, 50);
-                    Thread.Sleep(50);
-                }
-                foreach (var position in Position.Hexagon.All.AsEnumerable().Reverse())
-                {
-                    SequentialCommandOneShotSmoothly(position, OperationTarget.Both, 50);
-                    Thread.Sleep(200);
-                }
-            });
-        }
-
-        private async void SampleCircle()
-        {
-            await Task.Run(() =>
-            {
-                CommandRegister();
-                {
-
-                    for (int i = 500; i > 0; i /= 2)
-                    {
-                        SequentialCommandOneShotSmoothly(new List<uint> { Position.Hexagon.Up, Position.Hexagon.Down }, OperationTarget.TileOnly, 10);
-                        Thread.Sleep(i);
-                        SequentialCommandOneShotSmoothly(new List<uint> { Position.Hexagon.RightUp, Position.Hexagon.LeftDown }, OperationTarget.TileOnly, 10);
-                        Thread.Sleep(i);
-                        SequentialCommandOneShotSmoothly(new List<uint> { Position.Hexagon.RightDown, Position.Hexagon.LeftUp }, OperationTarget.TileOnly, 10);
-                        Thread.Sleep(i);
-                    }
-                }
-
-                CommandTurnOffAll();
-            });
-        }
-
-        private async void InternalMultiThreadTest()
-        {
-            await Task.Run(() =>
-            {
-                foreach (var position in Position.Hexagon.Surroundings)
-                {
-                    SequentialCommandTurnOnSmoothly(position, OperationTarget.TileOnly, 50);
-                }
-
-                foreach (var position in Position.Hexagon.Surroundings)
-                {
-                    SequentialCommandTurnOffSmoothly(position, OperationTarget.TileOnly, 50);
-                }
-            });
-        }
-
-        private async void SampleStress()
-        {
-            // [性能メモ]
-            // - 送信回数: 28014 (回)
-            // - 経過時間: 55.5  (秒)
-            // - 115200bps で帯域占有が約 70 %
-            // - I2C バスは帯域余ってるっぽいので
-            //   シリアル通信速度を上げるのが吉
-            await Task.Run(() =>
-            {
-                CommandRegister();
-                for (int i = 0; i < 1000; i++)
-                {
-                    CommandTurnOffAll();
-                    CommandTurnOnAll();
-                }
-            });
         }
     }
 }
