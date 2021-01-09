99f1afcd1ee4676a179808c6c7984c88478532c2
extsui <en422641@s.okayama-u.ac.jp>
2021-01-09 22:49:58 +0900

    (PatternConverter) 自動生成ファイルを登録 #38

---
+++ PRG/Tools/PatternConverter/Outputs/pattern.js
@@ -0,0 +1,39 @@
+/* Ç±ÇÃÉtÉ@ÉCÉãÇÕ PatternConverter Ç…ÇÊÇ¡Çƒé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ */
+var patternList = {
+    // Grass
+    "00": { text: "Grass_AllOn", stepCount: 1, stepTiming: 10, isRepeat: false },
+    "01": { text: "Grass_AllOff", stepCount: 1, stepTiming: 10, isRepeat: false },
+    "02": { text: "Grass_LeftToRight1", stepCount: 12, stepTiming: 10, isRepeat: false },
+    "03": { text: "Grass_RightToLeft1", stepCount: 12, stepTiming: 10, isRepeat: false },
+    "04": { text: "Grass_LeftToRight3", stepCount: 14, stepTiming: 10, isRepeat: false },
+    "05": { text: "Grass_RightToLeft3", stepCount: 14, stepTiming: 10, isRepeat: false },
+    "06": { text: "Grass_BothEdgeToMiddle", stepCount: 12, stepTiming: 10, isRepeat: false },
+    "07": { text: "Grass_Vibration", stepCount: 2, stepTiming: 10, isRepeat: false },
+    "08": { text: "Grass_LeftToRightBuffer", stepCount: 134, stepTiming: 10, isRepeat: false },
+    "09": { text: "Grass_LeftToRightNeg", stepCount: 14, stepTiming: 10, isRepeat: false },
+    "10": { text: "Grass_RightToLeftNeg", stepCount: 14, stepTiming: 10, isRepeat: false },
+    "11": { text: "Grass_LeftToRightVertical", stepCount: 8, stepTiming: 10, isRepeat: false },
+    "12": { text: "Grass_RightToLeftVertical", stepCount: 8, stepTiming: 10, isRepeat: false },
+    // House
+    "00": { text: "House_AllOn", stepCount: 1, stepTiming: 10, isRepeat: false },
+    "01": { text: "House_AllOff", stepCount: 1, stepTiming: 10, isRepeat: false },
+    "02": { text: "House_OneByOne", stepCount: 28, stepTiming: 10, isRepeat: false },
+    "03": { text: "House_Stream", stepCount: 54, stepTiming: 10, isRepeat: false },
+    // Tile
+    "00": { text: "Tile_AllOn", stepCount: 1, stepTiming: 10, isRepeat: false },
+    "01": { text: "Tile_AllOff", stepCount: 1, stepTiming: 10, isRepeat: false },
+    "02": { text: "Tile_OneByOne", stepCount: 24, stepTiming: 10, isRepeat: false },
+    "03": { text: "Tile_Stream", stepCount: 48, stepTiming: 10, isRepeat: false },
+    // Tree
+    "00": { text: "Tree_AllOn", stepCount: 1, stepTiming: 10, isRepeat: false },
+    "01": { text: "Tree_AllOff", stepCount: 1, stepTiming: 10, isRepeat: false },
+    "02": { text: "Tree_OneByOne", stepCount: 20, stepTiming: 10, isRepeat: false },
+    "03": { text: "Tree_TopToBottomHorizontal1", stepCount: 8, stepTiming: 10, isRepeat: false },
+    "04": { text: "Tree_TopToBottomHorizontal3", stepCount: 10, stepTiming: 10, isRepeat: false },
+    "05": { text: "Tree_TopToBottomBuffer", stepCount: 14, stepTiming: 10, isRepeat: false },
+    "06": { text: "Tree_UpperLeftToRight", stepCount: 6, stepTiming: 10, isRepeat: false },
+    "07": { text: "Tree_UpperRightToLeft", stepCount: 6, stepTiming: 10, isRepeat: false },
+    "08": { text: "Tree_LowerLeftToRight", stepCount: 6, stepTiming: 10, isRepeat: false },
+    "09": { text: "Tree_LowerRightToLeft", stepCount: 6, stepTiming: 10, isRepeat: false },
+    "10": { text: "Tree_Candle", stepCount: 22, stepTiming: 10, isRepeat: false },
+};
