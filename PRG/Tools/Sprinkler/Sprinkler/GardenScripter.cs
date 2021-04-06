ea6c4e177efbc05764e1758d8be5940c150d1d74
extsui <en422641@s.okayama-u.ac.jp>
2021-04-06 22:34:36 +0900

    (Sprinkler) 各部品指定方法の追加と部品テスト実装 #45

--- PRG/Tools/Sprinkler/Sprinkler/GardenScripter.cs
+++ PRG/Tools/Sprinkler/Sprinkler/GardenScripter.cs
@@ -39,7 +39,7 @@ namespace Sprinkler
         }
 
         /// <summary>
-        /// 指定あれたアクションを非同期実行するラッパー
+        /// 指定されたアクションを非同期実行するラッパー
         /// 対象のアクションは async 指定が不要
         /// </summary>
         private async void RunSequenceAsync(Action action)
@@ -350,14 +350,71 @@ namespace Sprinkler
                     PatternConstants.Grass.RightToLeftVertical,
                 };
 
-                foreach (var pattern in GrassPatternList)
+                var HousePatternList = new List<PatternConstants.Pattern>()
                 {
-                    var patternCommandArgs = new BrickCommandArgs.Pattern(pattern.Id, StepTimingMilliseconds, false);
-                    ExecuteCommand(m_garden.MakePatternCommand(Position.Hexagon.All, OperationTarget.Both, patternCommandArgs));
+                    PatternConstants.House.AllOn,
+                    PatternConstants.House.AllOff,
+                    PatternConstants.House.OneByOne,
+                    PatternConstants.House.Stream,
+                };
 
-                    // パターンが終わるまで待つ
-                    Thread.Sleep(pattern.GetTime(StepTimingMilliseconds));
-                }
+                var TilePatternList = new List<PatternConstants.Pattern>()
+                {
+                    PatternConstants.Tile.AllOn,
+                    PatternConstants.Tile.AllOff,
+                    PatternConstants.Tile.OneByOne,
+                    PatternConstants.Tile.Stream,
+                };
+
+                var TreePatternList = new List<PatternConstants.Pattern>()
+                {
+                    PatternConstants.Tree.AllOn,
+                    PatternConstants.Tree.AllOff,
+                    PatternConstants.Tree.OneByOne,
+                    PatternConstants.Tree.TopToBottomHorizontal1,
+                    PatternConstants.Tree.TopToBottomHorizontal3,
+                    PatternConstants.Tree.TopToBottomBuffer,
+                    PatternConstants.Tree.UpperLeftToRight,
+                    PatternConstants.Tree.UpperRightToLeft,
+                    PatternConstants.Tree.LowerLeftToRight,
+                    PatternConstants.Tree.LowerRightToLeft,
+                    PatternConstants.Tree.Candle,
+                };
+
+                // 全消灯
+                ExecuteCommand(m_garden.MakePatternCommand(Position.Hexagon.All, OperationTarget.Both, BrickCommandArgs.PatternTurnOff));
+                Thread.Sleep(500);
+
+                // 全点灯
+                ExecuteCommand(m_garden.MakePatternCommand(Position.Hexagon.All, OperationTarget.Both, BrickCommandArgs.PatternTurnOn));
+                Thread.Sleep(500);
+
+                // 全消灯
+                ExecuteCommand(m_garden.MakePatternCommand(Position.Hexagon.All, OperationTarget.Both, BrickCommandArgs.PatternTurnOff));
+                Thread.Sleep(500);
+
+                Action<List<PatternConstants.Pattern>, OperationTarget> TestBrickPattern = (patterns, targets) =>
+                {
+                    foreach (var pattern in patterns)
+                    {
+                        var patternCommandArgs = new BrickCommandArgs.Pattern(pattern.Id, StepTimingMilliseconds, false);
+                        ExecuteCommand(m_garden.MakePatternCommand(Position.Hexagon.All, targets, patternCommandArgs));
+
+                        // パターンが終わるまで待つ
+                        Thread.Sleep(pattern.GetTime(StepTimingMilliseconds));
+                    }
+                };
+
+                Action TestGrassPattern = () => TestBrickPattern(GrassPatternList, OperationTarget.GrassOnly);
+                Action TestHousePattern = () => TestBrickPattern(HousePatternList, OperationTarget.HouseOnly);
+                Action TestTilePattern = () => TestBrickPattern(TilePatternList, OperationTarget.TileOnly);
+                Action TestTreePattern = () => TestBrickPattern(TreePatternList, OperationTarget.TreeOnly);
+
+                // ここから並列実行
+                RunSequenceAsync(TestGrassPattern);
+                RunSequenceAsync(TestHousePattern);
+                RunSequenceAsync(TestTilePattern);
+                RunSequenceAsync(TestTreePattern);
             });
         }
     }
