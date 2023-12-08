02efe35dc1ec21b27f9c7e744c275dac91aaf0a8
extsui <en422641@s.okayama-u.ac.jp>
2023-12-08 23:00:10 +0900

    (BuildingPainter) WebSocket 受信データをシリアルに送信する処理を作成

--- PRG/Tools/BuildingPainter/app.py
+++ PRG/Tools/BuildingPainter/app.py
@@ -1,8 +1,30 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import serial
+import sys
+import threading
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 socketio = SocketIO(app)
+
+ser = serial.Serial('COM4', 115200)
+
+def serial_listener():
+    while True:
+        try:
+            # シリアルポートからデータを受信
+            data = ser.readline().decode('utf-8').strip()
+            # 受信したデータを Flask サーバーのログに出力
+            print(f'{data}')
+        except Exception as e:
+            print(f'[serial] Serial Error: {e}')
+            ser.close()
+            sys.exit()
+
+# シリアルリスナーをスレッドで実行
+serial_thread = threading.Thread(target=serial_listener)
+serial_thread.daemon = True
+serial_thread.start()
 
 @app.route('/')
 def index():
@@ -12,6 +34,8 @@ def index():
 def handle_message(message):
     # クライアントからのメッセージ受信
     print('Received message:', message)
+    ser.write(('@' + message + '\n').encode())
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    # シリアルポートへのアクセスを可能にするために use_reloader=False を指定
+    socketio.run(app, debug=True, use_reloader=False)
